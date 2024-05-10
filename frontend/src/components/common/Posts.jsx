95fd684b5f61a4ac2b8c5cb6d53fc8fabe7c3d7a
Nnamdi Cyprian Ekechi <nnamdi4u09@gmail.com>
2024-05-10 19:39:22 +0100

    Profile page posts and info fetched

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -4,37 +4,46 @@ import PostSkeleton from "../skeletons/PostSkeleton";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
-const Posts = ({feedType}) => {
-
+const Posts = ({ feedType, username, userId }) => {
   const getPostEndpoint = () => {
     switch (feedType) {
       case "forYou":
         return "/api/posts/all";
       case "following":
         return "/api/posts/following";
+      case "posts":
+        return `/api/posts/user/${username}`;
+      case "likes":
+        return `/api/posts/likes/${userId}`;
       default:
         return "/api/posts/all";
     }
-  }
+  };
 
   const POST_ENDPOINT = getPostEndpoint();
 
-  const {data: POSTS, isLoading, refetch, isRefetching} = useQuery({
+  const {
+    data: POSTS,
+    isLoading,
+    refetch,
+    isRefetching,
+  } = useQuery({
     queryKey: ["posts"],
     queryFn: async () => {
-     try {
-       const res = await fetch(POST_ENDPOINT);
-       const data = await res.json();
-       if (!res.ok) throw new Error(data.error || "Something went wrong");
-       return data;
-     } catch (error) {
-       throw new Error(error);
-     }
-  }})
+      try {
+        const res = await fetch(POST_ENDPOINT);
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.error || "Something went wrong");
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
 
   useEffect(() => {
-     refetch();
-  }, [feedType, refetch]);
+    refetch();
+  }, [feedType, refetch, username, userId]);
 
   return (
     <>
@@ -46,7 +55,7 @@ const Posts = ({feedType}) => {
         </div>
       )}
       {!isLoading && !isRefetching && POSTS?.length === 0 && (
-        <p className="text-center my-4">No posts in this tab. Switch 👻</p>
+        <p className="text-center my-4">This user has no posts</p>
       )}
       {!isLoading && !isRefetching && POSTS && (
         <div>
